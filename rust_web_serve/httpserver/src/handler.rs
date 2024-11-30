34344fdba467790453da854b59b69cda5db7f149
Memoyu <mmy6076@outlook.com>
2024-11-30 11:10:12 +0800

    完成案例

--- rust_web_serve/httpserver/src/handler.rs
+++ rust_web_serve/httpserver/src/handler.rs
@@ -59,3 +59,32 @@ impl Handler for StaticPageHandler {
         }
     }
 }
+
+impl WebServiceHandler {
+    fn load_json() -> Vec<OrderStatus> {
+        let default_path = format!("{}/data", env!("CARGO_MANIFEST_DIR"));
+        let data_path = env::var("DATA_PATH").unwrap_or(default_path);
+        let full_path = format!("{}/{}", data_path, "orders.json");
+        let json_contents = fs::read_to_string(full_path);
+        let orders: Vec<OrderStatus> =
+            serde_json::from_str(json_contents.unwrap().as_str()).unwrap();
+        orders
+    }
+}
+
+impl Handler for WebServiceHandler {
+    fn handle(req: &HttpRequest) -> HttpResponse {
+        let http::httprequest::Resource::Path(s) = &req.resource;
+        let route: Vec<&str> = s.split("/").collect();
+
+        match route[2] {
+            "shopping" if route.len() > 2 && route[3] == "order" => {
+                let body = Some(serde_json::to_string(&Self::load_json()).unwrap());
+                let mut headers: HashMap<&str, &str> = HashMap::new();
+                headers.insert("Content-Type", "application/json");
+                HttpResponse::new("200", Some(headers), body)
+            }
+            _ => HttpResponse::new("404", None, Self::load_file("404.html")),
+        }
+    }
+}
--- rust_web_serve/httpserver/src/router.rs
+++ rust_web_serve/httpserver/src/router.rs
@@ -7,7 +7,21 @@ pub struct Router;
 impl Router {
     pub fn route(req: HttpRequest, stream: &mut impl Write) -> () {
         match req.method {
-            httprequest::Method::Get => match &&req.resource {},
+            httprequest::Method::Get => match &req.resource {
+                httprequest::Resource::Path(s) => {
+                    let route: Vec<&str> = s.split("/").collect();
+                    match route[1] {
+                        "api" => {
+                            let res = WebServiceHandler::handle(&req);
+                            let _ = res.send_response(stream);
+                        }
+                        _ => {
+                            let resp: HttpResponse = StaticPageHandler::handle(&req);
+                            let _ = resp.send_response(stream);
+                        }
+                    }
+                }
+            },
             _ => {
                 let resp: HttpResponse = PageNotFoundHandler::handle(&req);
                 let _ = resp.send_response(stream);
